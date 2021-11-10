e233eff1d10b9f10613417a22fdafdc89a1c103d
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-11-10 17:52:34 +0100

    bak end auth done

--- models/Project.model.js
+++ models/Project.model.js
@@ -6,8 +6,9 @@ const { Schema, model } = mongoose;
 const projectSchema = new Schema({
   title: String,
   description: String,
-  tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }]
+  tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
   // owner will be added later on
+  owner: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports = model('Project', projectSchema);
